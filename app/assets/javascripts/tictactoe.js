21aac0c324bf7c8bf9a99063ef9b6ca28a7977ee
Brie Coyle <brie.coyle@gmail.com>
2017-05-14 13:47:47 -0400

    Error after merge conflicts.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -25,80 +25,16 @@ function attachListeners() {
     e.preventDefault();
     doTurn(e)
   })
-<<<<<<< HEAD
 }
 
 var player = function() {
   return (turn % 2 === 0) ? "O" : "X"; //if no remainder then x
 }
-=======
-}
-
-function player() {
-  return (turn % 2 === 0) ? "O" : "X"; //if no remainder then x
-}
->>>>>>> 45bda08d100102acc7a1cf20d58a7a6260ef985c
 
 function doTurn(e) {
   turn += 1
   updateState(e)
   checkWinner()
-}
-
-var updateState = function(e) {
-  $(e.currentTarget).html(player());
-}
-
-function checkWinner() {
-  var currentPlayer = player()
-  var board = getBoard();
-  winningCombos.forEach(function(combo) { //iterates over Combos array
-    if (board[combo[0]] == currentPlayer && board[combo[1]] == currentPlayer && board[combo[2]] == currentPlayer) {
-      message("Player " + currentPlayer + " Won!"); //calls message() based on current player
-      clearBoard();
-    } else if (turn === 9) {
-      message("Tie game")
-    }
-  })
-}
-
-// function checkWinner() { //I started building this - not sure it's on the right track yet
-//   var board = getBoard();
-//   winningCombos.forEach(function(combo) { //iterates over Combos array
-//     if ((board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") || (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")) {
-//       message("Player " + player() + " Won!"); //calls message() based on current player
-//       clearBoard();
-//     } else if (boardFull(board)){
-//       message("Tie game")
-//     }
-//   })
-// }
-//
-function getBoard() {
-  var board = [];
-  $td = $("td");
-  for (var i=0; i < 9; i++) {
-    var cell = $td[i];
-    board.push(cell.innerHTML);
-    }
-  return board;
-}
-//
-// function boardFull(board) {
-//   if (board.includes("")) {
-//     return false
-//   } else {
-//     return true
-//   }
-// }
-//
-function clearBoard() {
-  $("td").html("");
-  turn = 0; //restarts games
-}
-//
-function message(string) {
-  $("div#message").text(string) //edited to replace text instead of append
 }
 
 var updateState = function(e) {
